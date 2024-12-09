cadb05f4a729cc883b1be9197365796abf175496
Irmak Erkol <irmak.erkol@sabanciuniv.edu>
2024-12-10 00:24:12 +0300

    navbar added

--- Assets/Scripts/PanelManager.cs
+++ Assets/Scripts/PanelManager.cs
@@ -3,21 +3,31 @@ using UnityEngine.UI;
 
 public class PanelController : MonoBehaviour
 {
-    public GameObject mainMenuPanel,galleryPanel,favoritesPanel,cameraPanel;
-    public Button galleryButton,favoritesButton,cameraButton;
+    public GameObject mainMenuPanel,galleryPanel,favoritesPanel,cameraPanel,helpPanel, navBarPanel;
+    public Button galleryButton,favoritesButton,cameraButton,helpButton;
+
+    public Button infoButton, menuButton;
 
     private void Start()
     {
         galleryButton.onClick.AddListener(ShowGallery);
         favoritesButton.onClick.AddListener(ShowFavorites);
         cameraButton.onClick.AddListener(ShowTakePhoto);
+        helpButton.onClick.AddListener(ShowHelp);
+        infoButton.onClick.AddListener(ShowHelp);
+        menuButton.onClick.AddListener(ShowMainMenu);
     }
 
-    private void OnDestroy() {
+    private void OnDestroy() 
+    {
         galleryButton.onClick.RemoveListener(ShowGallery);
         favoritesButton.onClick.RemoveListener(ShowFavorites);
         cameraButton.onClick.RemoveListener(ShowTakePhoto);
+        helpButton.onClick.RemoveListener(ShowHelp);
+        infoButton.onClick.RemoveListener(ShowHelp);
+        menuButton.onClick.RemoveListener(ShowMainMenu);
     }
+
     private void Update()
     {
         
@@ -33,6 +43,17 @@ public class PanelController : MonoBehaviour
 
     }
 
+    public void ShowHelp()
+    {
+        SoundManager.instance.PlaySound(SoundManager.instance.GetAudioClip(SoundManager.Sound.ButtonClick));
+        mainMenuPanel.SetActive(false);
+        galleryPanel.SetActive(false);
+        favoritesPanel.SetActive(false);
+        cameraPanel.SetActive(false);
+        helpPanel.SetActive(true);
+        navBarPanel.SetActive(false);
+    }
+
     public void ShowMainMenu()
     {
         SoundManager.instance.PlaySound(SoundManager.instance.GetAudioClip(SoundManager.Sound.ButtonClick));
@@ -40,6 +61,8 @@ public class PanelController : MonoBehaviour
         galleryPanel.SetActive(false);
         favoritesPanel.SetActive(false);
         cameraPanel.SetActive(false);
+        helpPanel.SetActive(false);
+        navBarPanel.SetActive(false);
     }
 
     public void ShowGallery()
@@ -49,6 +72,8 @@ public class PanelController : MonoBehaviour
         galleryPanel.SetActive(true);
         favoritesPanel.SetActive(false);
         cameraPanel.SetActive(false);
+        helpPanel.SetActive(false);
+        navBarPanel.SetActive(true);
 
         EventService.OpenGallery();
     }
@@ -60,6 +85,8 @@ public class PanelController : MonoBehaviour
         galleryPanel.SetActive(false);
         favoritesPanel.SetActive(true);
         cameraPanel.SetActive(false);
+        helpPanel.SetActive(false);
+        navBarPanel.SetActive(true);
     }
 
     public void ShowTakePhoto()
@@ -69,6 +96,8 @@ public class PanelController : MonoBehaviour
         galleryPanel.SetActive(false);
         favoritesPanel.SetActive(false);
         cameraPanel.SetActive(true);
+        helpPanel.SetActive(false);
+        navBarPanel.SetActive(true);
     }
 
     public void ExitApplication()
